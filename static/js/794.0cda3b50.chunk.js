635e422db195d5716fe6c17a264b1e681ad4c042
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2024-01-30 17:08:49 +0000

    Deploying to gh-pages from @ djurik2008/goit-react-hw-05-movies@325d9cf5fe51f6b5ead94aeee2b6deca2cfe0a6f 🚀

---
+++ static/js/794.0cda3b50.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{794:function(e,t,r){r.r(t);var n=r(87),o=r(184);t.default=function(){return(0,o.jsxs)("div",{children:[(0,o.jsx)("h1",{children:"404 Page not found"}),(0,o.jsx)(n.rU,{to:"/",children:"...to Home page"})]})}}}]);
+//# sourceMappingURL=794.0cda3b50.chunk.js.map
